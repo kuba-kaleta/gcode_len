a9dc34c3fb031366b157a446156cf3c24b9460b2
kuba-kaleta <kubamen9@wp.pl>
2022-12-28 15:10:39 +0100

    g0, g1, g3 functions

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,16 +1,24 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Objects;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class ReadFile {
+
+    static int cont = 0;
     public static void read() {
         try {
             File myObj = new File("D:\\jakub.kaleta\\isys\\dlugosc_ciecia\\katownik-01.ncp");
             Scanner myReader = new Scanner(myObj);
+
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                if(data.charAt(0) == 'N'){
 
+                if(Objects.equals(data, "(<Contour>)")){
+                    cont += 1;
+                }
+                if(data.charAt(0) == 'N'){
+                    Gcode.gcode_parse(data);
                 }
                 // System.out.println(data);
             }
