92a4db2f4ba5f0d90d917c65010373ac332195a2
kuba-kaleta <kubamen9@wp.pl>
2023-05-25 11:23:42 +0200

    finished alogorithm

--- Gcode.py
+++ Gcode.py
@@ -6,14 +6,12 @@ class Gcode:
     pi = 3.14159265359
     posx = 0
     posy = 0
+
     @staticmethod
     def gcode_parse(data):
         lett = 'Q'
         num = ""
         i = 0
-        # Gcode.numbers['I'] = 0.0
-        # Gcode.numbers['J'] = 0.0
-        # Gcode.numbers['G'] = 0.5
         while i < len(data):
             c = data[i]
             if 'A' <= c <= 'Z':
@@ -22,8 +20,6 @@ class Gcode:
                     Gcode.numbers[lett] = f
                 lett = c
                 num = ""
-                if c == 'G':
-                    wpisg = True
 
             if ('0' <= c <= '9') or (c == '.'):
                 num += c
@@ -34,17 +30,12 @@ class Gcode:
         g = Gcode.numbers['G']
         if g == 1.0:
             return Gcode.g1()
-        elif g == 2.0:
-            return Gcode.g2()
-        elif g == 3.0:
-            return Gcode.g3()
+        elif g == 2.0 or g == 3.0:
+            return Gcode.g2g3()
         else:
             Gcode.posx = Gcode.numbers['X']
             Gcode.posy = Gcode.numbers['Y']
             return 0.0
-
-    # @staticmethod
-    # def gx():
 
     @staticmethod
     def g1():
@@ -57,12 +48,12 @@ class Gcode:
         # print("g1 " + str(math.sqrt(re)))
 
     @staticmethod
-    def g2():
+    def g2g3():
         difx = Gcode.numbers['X'] - Gcode.posx
         dify = Gcode.numbers['Y'] - Gcode.posy
+        P23 = math.sqrt(difx ** 2 + dify ** 2)
         Gcode.posx = Gcode.numbers['X']
         Gcode.posy = Gcode.numbers['Y']
-        P23 = math.sqrt(difx ** 2 + dify ** 2)
         difxr = Gcode.numbers['I'] - Gcode.posx
         difyr = Gcode.numbers['J'] - Gcode.posy
         r = math.sqrt(difxr ** 2 + difyr ** 2)
@@ -76,27 +67,4 @@ class Gcode:
             ang = math.degrees(math.acos(arcarg))
         return ang * per / 360
         # print("g3 " + str(ang * per / 360))
-
-    @staticmethod
-    def g3():
-        difx = Gcode.numbers['X'] - Gcode.posx
-        dify = Gcode.numbers['Y'] - Gcode.posy
-        Gcode.posx = Gcode.numbers['X']
-        Gcode.posy = Gcode.numbers['Y']
-        P23 = math.sqrt(difx ** 2 + dify ** 2)
-        difxr = Gcode.numbers['I'] - Gcode.posx
-        difyr = Gcode.numbers['J'] - Gcode.posy
-        r = math.sqrt(difxr ** 2 + difyr ** 2)
-        per = 2 * Gcode.pi * r
-        arcarg = (r ** 2 + r ** 2 - P23 ** 2) / (2 * r * r)
-        if arcarg > 1:
-            ang = math.degrees(math.acos(1))
-        elif arcarg < -1:
-            ang = math.degrees(math.acos(-1))
-        else:
-            ang = math.degrees(math.acos(arcarg))
-        return (360 - ang) * per / 360
-        # print("g2 " + str(ang * per / 360))
-
-
 
--- ReadFile.py
+++ ReadFile.py
@@ -20,25 +20,24 @@ class ReadFile:
                         ReadFile.cont = 1
                     if i.find('Contour') == 3:  # (</Contour>)
                         ReadFile.cont = 0
-                        print("Contour: " + format(ReadFile.resp, '.2f') + " Suma: " + format(ReadFile.res, '.2f'))
-                        ReadFile.resp = 0.0
+                        # print("Contour: " + format(ReadFile.resp, '.2f') + " Suma: " + format(ReadFile.res, '.2f'))
+                        # ReadFile.resp = 0.0
                     if i.find('Plan') == 2:  # (<Plan>)
                         print('==========================================\nFile name: ' + filename)
                         if i.find('JobCode') >= 0:
                             print(i[7:-3])
                     if i.find('Part') == 2:  # (<Part>)
-                        ReadFile.cont += 1
                         ReadFile.part = i[17:-14]
                     if i[0] == 'N':
                         skl = Gcode.Gcode.gcode_parse(i)
                         if ReadFile.cont == 1:
                             # print(skl)
                             ReadFile.res += skl
-                            ReadFile.resp += skl
-                    if i.find('Part') == 3:  # (</Plan>)
+                            # ReadFile.resp += skl
+                    if i.find('Part') == 3:  # (</Part>)
                         print("Part name: " + ReadFile.part + ": " + format(ReadFile.res, '.2f') + " mm")
                         ReadFile.res = 0.0
-                        ReadFile.resp = 0.0
+                        # ReadFile.resp = 0.0
                         ReadFile.cont = 0
                     if i.find('NcpProgram') == 3:
                         print('==========================================\n')
